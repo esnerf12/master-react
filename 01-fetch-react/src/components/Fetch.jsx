294f1c54fc8fa9d9c518f6c0d97b4cedb8c989e2
Anderson Tovar <andersontovar09@gmail.com>
2023-11-20 19:26:09 -0400

    fetch and fetch async await

---
+++ 01-fetch-react/src/components/Fetch.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react"
+
+export function Fetch () {
+    const [ users, setUsers ] = useState([])
+
+/*     const getUsers = () => {
+        fetch("https://reqres.in/api/users?page=2")
+            .then(res => res.json())
+            .then(res => setUsers(res.data))
+    } */
+
+    const getUsersAW = async () => {
+        const res = await fetch("https://reqres.in/api/users?page=2")
+        const {data} = await res.json()
+        setUsers(data)
+    }
+
+    useEffect(() => {
+        /* getUsers() */
+        getUsersAW()
+    }, [])
+
+    return (
+        <>
+            {
+                users.map(user => {
+                    return (
+                        <div key={user.id}>
+                            <img src={ user.avatar } alt="avatar" width="80" />
+                            <li>
+                                <strong>fullname:</strong> { user.first_name } { user.last_name } <br /> <strong>email:</strong> { user.email }
+                            </li>
+                        </div>
+                    )
+                })
+            }
+        </>
+    )
+}
